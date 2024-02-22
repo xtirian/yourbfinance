27c82dfd1d9adbbf7d8115762da5d4435d299466
xTirian <tirianx@yahoo.com>
2024-02-22 20:57:01 -0300

    refactor: criar repository em layout de classe

--- backend/src/repository/user.respository.ts
+++ backend/src/repository/user.respository.ts
@@ -2,18 +2,16 @@ import { type Prisma } from '@prisma/client';
 import { prisma } from '../config/index';
 import { type UserModel } from '../model/user.model';
 
-const UserRepository = {
-  findMany: async (params: Prisma.UserFindManyArgs): Promise<UserModel[]> => {
+export class UserRepository {
+  async findMany(params: Prisma.UserFindManyArgs): Promise<UserModel[]> {
     return await prisma.user.findMany({ ...params });
-  },
+  }
 
-  findUnique: async (params: Prisma.UserFindUniqueArgs): Promise<UserModel | null> => {
+  async findUnique(params: Prisma.UserFindUniqueArgs): Promise<UserModel | null> {
     return await prisma.user.findUnique({ ...params });
-  },
+  }
 
-  create: async (params: Prisma.UserCreateArgs): Promise<UserModel> => {
+  async create(params: Prisma.UserCreateArgs): Promise<UserModel> {
     return await prisma.user.create({ ...params });
   }
-};
-
-export default UserRepository;
+}
