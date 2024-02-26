1e2af9cf0eafb549c4d03300dcb1d6b946ed82e8
xTirian <tirianx@yahoo.com>
2024-02-26 08:41:39 -0300

    feat: config headers de requisição e resposta http

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -7,6 +7,15 @@ import swaggerFile from './doc/swagger-output.json';
 dotenv.config();
 const app: Express = express();
 app.use(express.json());
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', process.env.FRONTEND_URL ?? 'http://localhost:5173');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.header('Access-Control-Allow-Credentials', 'true');
+
+  // Passa para o próximo middleware
+  next();
+});
 
 const PORT: number | string = process.env.PORT ?? 4000;
 
